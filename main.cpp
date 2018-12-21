cc025a31410b7163cdfb9b53e20159db8d4ccc5e
gjspeak <gjspeak@163.com>
2018-12-21 10:14:30 +0800

    selection sort and insert sort

--- main.cpp
+++ main.cpp
@@ -2,34 +2,83 @@
 // Created by gjs on 18-10-18.
 //
 
-// 希尔排序
+// selection sort and insert sort
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-void Shell(vector<int>& vec) {
+template <typename T>
+void SelectionSort(vector<T>& vec) {
     int len = vec.size();
-    int h = 1;
-    while (h < len/3)
-        h = 3*h + 1;
-    while(h >= 1) {
-        for(int i=h; i<len; i++) {
-            int tt = vec[i], j=0;
-            for(j=i; j>=h && vec[j-h]>tt; j-=h) {
-                vec[j] = vec[j-h];
+    for(int i=0; i<len; i++) {
+        int min = i;
+        for(int j=i+1; j<len; j++) {
+            if(vec[j] < vec[min]) {
+                min = j;
             }
-            vec[j] = tt;
         }
-        h /= 3;
+        int temp = vec[min];
+        vec[min] = vec[i];
+        vec[i] = temp;
+    }
+}
+
+template <typename T>
+void InsertionSortSwap(vector<T>& vec) {
+    int len = vec.size();
+    for(int i=1; i<len; i++) {
+        for(int j=i; j>0; j--) {
+            if(vec[j] < vec[j-1]) {
+                T temp = vec[j];
+                vec[j] = vec[j-1];
+                vec[j-1] = temp;
+            }
+        }
+    }
+}
+
+template <typename T>
+void InsertionSortBack(vector<T>& vec) {
+    int len = vec.size();
+    for(int i=1; i<len; i++) {
+        T temp = vec[i];
+        int j;
+        for(j=i-1; j>=0 && (vec[j] > temp); j--) {
+            vec[j+1] = vec[j];
+        }
+        vec[j+1] = temp;
+    }
+}
+
+template <typename T>
+void InsertionSortWithGuard(vector<T>& vec) {
+    int len = vec.size(), min=0;
+    for(int k=0; k<len; k++) {
+        if(vec[k] < vec[min]) {
+            min = k;
+        }
+    }
+    T temp = vec[min];
+    vec[min] = vec[0];
+    vec[0] = temp;
+    for(int i=1; i<len; i++) {
+        T temp = vec[i];
+        int j;
+        for(j=i-1; j>=0; j--) {
+            if(vec[j] > temp)
+                vec[j+1] = vec[j];
+            else
+                break;
+        }
+        vec[j+1] = temp;
     }
 }
 
 int main() {
-    vector<int> vec{100, 4, 23, 14, 46, 90, 24, 54, 46, 20};
-    Shell(vec);
-    cout << endl << "result : " << endl;
+    vector<int> vec{1, 42, 6, 33, 2, 70, 1};
+    SelectionSort(vec);
     for(auto e : vec) {
         cout << e << " ";
     }
