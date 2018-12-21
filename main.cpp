84f89bb8fd779f51d0471a9fe94940715faa1649
gjspeak <gjspeak@163.com>
2018-12-21 09:54:27 +0800

    add shell sort

--- main.cpp
+++ main.cpp
@@ -2,50 +2,33 @@
 // Created by gjs on 18-10-18.
 //
 
-//自底向上的归并排序
+// 希尔排序
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-class Solution {
-public:
-    void GBSort(vector<int>& vec) {
-        temp = vec;
-        int len = vec.size();
-        for(int sz=1; sz<len; sz *= 2) {
-            for(int lo=0; lo<len-sz; lo += 2*sz) {
-                Merge(vec, lo, lo+sz-1, min(len-1, lo+2*sz-1));
+void Shell(vector<int>& vec) {
+    int len = vec.size();
+    int h = 1;
+    while (h < len/3)
+        h = 3*h + 1;
+    while(h >= 1) {
+        for(int i=h; i<len; i++) {
+            int tt = vec[i], j=0;
+            for(j=i; j>=h && vec[j-h]>tt; j-=h) {
+                vec[j] = vec[j-h];
             }
+            vec[j] = tt;
         }
+        h /= 3;
     }
-private:
-    void Merge(vector<int>& vec, int lo, int mid, int hi) {
-        if(lo >= hi)
-            return;
-        if(vec[mid] <= vec[mid+1])
-            return;
-        for(int i=lo; i<=hi; i++)
-            temp[i] = vec[i];
-        int i=lo, j=mid+1;
-        for(int k=lo; k<=hi; k++) {
-            if(i>mid)
-                vec[k] = temp[j++];
-            else if(j>hi)
-                vec[k] = temp[i++];
-            else if(temp[i] > temp[j])
-                vec[k] = temp[j++];
-            else
-                vec[k] = temp[i++];
-        }
-    }
-    vector<int> temp;
-};
+}
 
 int main() {
     vector<int> vec{100, 4, 23, 14, 46, 90, 24, 54, 46, 20};
-    Solution().GBSort(vec);
+    Shell(vec);
     cout << endl << "result : " << endl;
     for(auto e : vec) {
         cout << e << " ";
