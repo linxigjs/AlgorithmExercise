4bc04c7a38156169cf76a3fe092d250bdf1b6615
gjspeak <gjspeak@163.com>
2018-12-21 11:50:34 +0800

    add quick sort source

--- main.cpp
+++ main.cpp
@@ -6,79 +6,52 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 template <typename T>
-void SelectionSort(vector<T>& vec) {
-    int len = vec.size();
-    for(int i=0; i<len; i++) {
-        int min = i;
-        for(int j=i+1; j<len; j++) {
-            if(vec[j] < vec[min]) {
-                min = j;
+class Solution {
+public:
+    void QuickSort(vector<T>& vec) {
+//        random_shuffle(vec.begin(), vec.end());
+        Sort(vec, 0, vec.size()-1);
+    }
+private:
+    void Sort(vector<T>& vec, int lo, int hi) {
+        if(lo >= hi)
+            return;
+        int j = Partition(vec, lo, hi);
+        Sort(vec, lo, j-1);
+        Sort(vec, j+1, hi);
+    }
+    int Partition(vector<T>& vec, int lo, int hi) {
+        int i=lo, j=hi+1, v=vec[lo];
+        while(1) {
+            while(vec[++i] < v && i<=hi) {
+                ;
             }
-        }
-        int temp = vec[min];
-        vec[min] = vec[i];
-        vec[i] = temp;
-    }
-}
-
-template <typename T>
-void InsertionSortSwap(vector<T>& vec) {
-    int len = vec.size();
-    for(int i=1; i<len; i++) {
-        for(int j=i; j>0; j--) {
-            if(vec[j] < vec[j-1]) {
-                T temp = vec[j];
-                vec[j] = vec[j-1];
-                vec[j-1] = temp;
+            while(vec[--j] > v && j>lo) {
+                ;
             }
-        }
-    }
-}
-
-template <typename T>
-void InsertionSortBack(vector<T>& vec) {
-    int len = vec.size();
-    for(int i=1; i<len; i++) {
-        T temp = vec[i];
-        int j;
-        for(j=i-1; j>=0 && (vec[j] > temp); j--) {
-            vec[j+1] = vec[j];
-        }
-        vec[j+1] = temp;
-    }
-}
-
-template <typename T>
-void InsertionSortWithGuard(vector<T>& vec) {
-    int len = vec.size(), min=0;
-    for(int k=0; k<len; k++) {
-        if(vec[k] < vec[min]) {
-            min = k;
-        }
-    }
-    T temp = vec[min];
-    vec[min] = vec[0];
-    vec[0] = temp;
-    for(int i=1; i<len; i++) {
-        T temp = vec[i];
-        int j;
-        for(j=i-1; j>=0; j--) {
-            if(vec[j] > temp)
-                vec[j+1] = vec[j];
-            else
+            if(i >= j)
                 break;
+            Exchange(vec, i, j);
         }
-        vec[j+1] = temp;
+        Exchange(vec, lo, j);
+        return j;
     }
-}
+    void Exchange(vector<T>& vec, int l, int r) {
+        int t = vec[l];
+        vec[l] = vec[r];
+        vec[r] = t;
+    }
+};
 
 int main() {
-    vector<int> vec{1, 42, 6, 33, 2, 70, 1};
-    SelectionSort(vec);
+    vector<int> vec{33, 42, 6, 33, 2, 70, 1, 10, 40, 20, 33, 70};
+    vector<int> vec2{11,2,3,4,5,6,7,8,9,10};
+    Solution<int>().QuickSort(vec);
     for(auto e : vec) {
         cout << e << " ";
     }
