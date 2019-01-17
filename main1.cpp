c9e58ac303e3996e280590c4f8792b75a9f6097c
gjspeak <gjspeak@163.com>
2019-01-17 16:54:37 +0800

    add EdgeWeightedGraph

--- main1.cpp
+++ main1.cpp
@@ -2,18 +2,16 @@
 // Created by gjs on 19-1-7.
 //
 
-#include "DirectedDFS.h"
+#include "EdgeWeightedGraph.h"
 
+using namespace std;
 
 int main() {
-    Digraph gh("../tiny.txt");
-    vector<int> sources{1, 2, 6};
-    DirectedDFS reachable(gh, sources);
-    for(int i=0; i<gh.GetV(); i++) {
-        if(reachable.Marked(i))
-            cout << i << " ";
+    EdgeWeightedGraph ew("../tiny.txt");
+
+    for(auto e : ew.GetEdges()) {
+        cout << *e;
     }
     cout << endl;
-
     return 0;
 }
